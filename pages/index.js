f660bf5017598090f387884b22219f99ff99d0b1
Santiago Rivarola <srivarola@gmail.com>
2023-01-17 15:46:48 -0300

    getSession

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
+import { getSession } from 'next-auth/react'
 import Center from '@/components/Center'
 import Player from '@/components/Player'
 import Sidebar from '@/components/Sidebar'
 import Head from 'next/head'
-
 
 export default function Home() {
   return (
@@ -27,4 +27,15 @@ export default function Home() {
       </div>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session
+    }
+  }
 }
