0bb4dabcb7bf57f98d291053e237da7a9689357a
Santiago Rivarola <srivarola@gmail.com>
2023-01-17 15:56:40 -0300

    login page

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,11 @@
 import { getProviders, signIn } from 'next-auth/react'
 import Image from 'next/image'
+import Logo from 'https://links.papareact.com/9xl'
 
 const Login = ({providers}) => {
   return (
     <div className='flex flex-col items-center justify-center bg-black min-h-screen w-full'>
-        <Image className='w-52 mb-5' src='https://links.papareact.com/9xl' alt='spotifyLogo' />
+        <Image width={52} src={Logo} alt='spotifyLogo' />
     </div>
   )
 }
