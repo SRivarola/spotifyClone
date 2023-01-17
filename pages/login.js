5568bb379aa6ed3bb63ac93f1155cf8d6d1c670f
Santiago Rivarola <srivarola@gmail.com>
2023-01-17 16:04:36 -0300

    login page

--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,19 @@ import Logo from '../public/spotifyLogo.png'
 const Login = ({providers}) => {
   return (
     <div className='flex flex-col items-center justify-center bg-black min-h-screen w-full'>
-        <Image width={52} src={Logo} alt='spotifyLogo' />
+        <Image width={208} src={Logo} alt='spotifyLogo' />
+        {
+            Object.values(providers).map((provider) => (
+                <div key={provider.name}>
+                    <button 
+                        className='mt-5 bg-[#18d860] text-white font-semibold p-4 rounded-full'
+                        onClick={() => signIn(provider.id, { callbackUrl: '/'})}    
+                    >
+                        Login with {provider.name}
+                    </button>
+                </div>
+            ))
+        }
     </div>
   )
 }
