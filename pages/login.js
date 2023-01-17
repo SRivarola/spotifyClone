66ec9d6879361239e06840a854f76aa4fca86d01
Santiago Rivarola <srivarola@gmail.com>
2023-01-17 15:53:56 -0300

    loggin page

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,22 @@
-const Login = () => {
+import { getProviders, signIn } from 'next-auth/react'
+import Image from 'next/image'
+
+const Login = ({providers}) => {
   return (
-    <div>Login</div>
+    <div className='flex flex-col items-center justify-center bg-black min-h-screen w-full'>
+        <Image className='w-52 mb-5' src='https://links.papareact.com/9xl' alt='spotifyLogo' />
+    </div>
   )
 }
 export default Login
+
+export async function getServerSideProps(){
+
+    const providers = await getProviders()
+
+    return {
+        props: {
+            providers,
+        }
+    }
+}
